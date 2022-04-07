04cbe3bcf46edf432a8aa67cec8137f11c4cb2b3
garion.goubard <garion.goubard@etu.u-bordeaux.fr>
2022-04-07 16:01:15 +0200

    init

---
+++ index.ts
@@ -0,0 +1,94 @@
+import fs from "fs";
+
+const file: string[] = fs.readFileSync("dictionnaire.txt", 'utf-8').split("\n");
+
+function wordHas(word: string, ...str: string[]) {
+    for (const letter of str) {
+        if (!word.includes(letter)) {
+            return false;
+        }
+    }
+    return true;
+}
+
+function find(length: number, { start, end, has, hasNot }: {start: string, end: string, has: string[], hasNot: string[]}): (word: string) => boolean {
+    return (word: string) => {
+        if (word.length !== length) {
+            return false;
+        }
+        if (start && !word.startsWith(start)) {
+            return false;
+        }
+        // remove startWith from word
+        if (start) {
+            word = word.slice(start.length, word.length);
+        }
+        if (end && !word.endsWith(end)) {
+            return false;
+        }
+        if(end){
+            word = word.slice(0, word.length - end.length);
+        }
+        if (has && !wordHas(word, ...has)) {
+            return false;
+        }
+        if (hasNot && !wordHasNot(word, ...hasNot)) {
+            return false;
+        }
+        return true;
+    }
+}
+
+function wordHasNot(word: string, ...str: string[]) {
+    for (const letter of str) {
+        if (word.includes(letter)) {
+            return false;
+        }
+    }
+    return true;
+}
+
+import prompter from "prompt-sync";
+const prompt = prompter({sigint: true});
+
+function promptUntil(promptMessage: string, validator: (word: string) => boolean): string {
+    let input = "";
+    while (!validator(input)) {
+        input = prompt(promptMessage).trim().toLowerCase();
+    }
+    return input;
+}
+
+const inputSize = +promptUntil("Taille du mot: ", (input) => {
+    if(!input){
+        return false;
+    }
+    if(isNaN(+input)){
+        console.log("La taille entrée n'est pas un nombre");
+        return false;
+    }
+    return true;
+});
+
+const startWith = prompt("Le mot commencer par: ");
+
+let endWith = prompt("Le mot finis par: ");
+let has = (() => {
+    const input = prompt("Le mot contient les lettres suivantes (séparer par des espaces): ");
+    if(!input){
+        return [];
+    }
+    return input.split(/ +/);
+})();
+let hasNot = (() => {
+    const input = prompt("Le mot ne contient pas les lettres suivantes (séparer par des espaces): ");
+    if(!input){
+        return [];
+    }
+    return input.split(/ +/);
+})();
+
+const guess = file.filter(find(inputSize, { start: startWith, end: endWith, has: has, hasNot: hasNot }));
+console.log("Le mot pourrait être:");
+console.log(guess);
+console.log("Il y a " + guess.length + " mots possibles");
