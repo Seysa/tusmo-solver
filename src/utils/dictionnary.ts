5c5b2c3b422633ac793df67cb03f0ee59700168e
Seysa <garion.g@hotmail.fr>
2022-09-22 10:40:13 +0200

    format dict

---
+++ src/utils/dictionnary.ts
@@ -0,0 +1,62 @@
+import fs from "fs";
+
+const file: string[] = fs.readFileSync("dictionnaire.txt", "utf-8").split("\n");
+
+function wordHas(word: string, ...str: string[]) {
+  function removeIndexFromWord(index: number) {
+    return word.slice(0, index) + word.slice(index + 1);
+  }
+  for (const letter of str) {
+    const index = word.indexOf(letter);
+    if (index === -1) {
+      return false;
+    }
+    word = removeIndexFromWord(index);
+  }
+  return true;
+}
+
+function find(
+  length: number,
+  {
+    start,
+    end,
+    has,
+    hasNot,
+  }: { start: string; end: string; has: string[]; hasNot: string[] }
+): (word: string) => boolean {
+  return (word: string) => {
+    if (word.length !== length) {
+      return false;
+    }
+    if (start && !word.startsWith(start)) {
+      return false;
+    }
+    // remove startWith from word
+    if (start) {
+      word = word.slice(start.length, word.length);
+    }
+    if (end && !word.endsWith(end)) {
+      return false;
+    }
+    if (end) {
+      word = word.slice(0, word.length - end.length);
+    }
+    if (has && !wordHas(word, ...has)) {
+      return false;
+    }
+    if (hasNot && !wordHasNot(word, ...hasNot)) {
+      return false;
+    }
+    return true;
+  };
+}
+
+function wordHasNot(word: string, ...str: string[]) {
+  for (const letter of str) {
+    if (word.includes(letter)) {
+      return false;
+    }
+  }
+  return true;
+}
